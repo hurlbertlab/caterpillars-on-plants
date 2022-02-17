582de79917683f82f6a85d3ac291bafe9f652b17
colleenw17 <cw17@live.unc.edu>
2022-02-17 17:45:34 -0500

    modifying r script

--- data/plant_species.r
+++ data/plant_species.r
@@ -1,5 +1,5 @@
 # This script automatically matches plant species names provided by users
-
+# Colleen is modifying this r.script to use
 
 library(dplyr)
 library(taxize)
@@ -7,6 +7,78 @@ library(stringr)
 
 sites = read.csv(list.files()[grep('Site.csv', list.files())], header = TRUE, stringsAsFactors = FALSE)
 plants = read.csv(list.files()[grep('Plant.csv', list.files())], header = TRUE, stringsAsFactors = FALSE)
+
+CurrentList <- as.data.frame(X2021_12_05_Plant) 
+plantname=as.data.frame(unique(CurrentList$Species))
+names(plantname) <-'plantName'
+
+
+cleanednewSpeciesList <- plantname %>%
+  mutate(cleanedPlantName = case_when(
+    plantName == "American witch-hazel" ~ "witch-hazel",
+    plantName == "American yellowwood" ~ "Kentucky yellowwood",
+    plantName == "Arrowwood" ~ "Viburnum",
+    plantName == "Arrowwood viburnum" ~ "Viburnum",
+    plantName == "Big-leaf dogwood" ~ "Cornus macrophylla",
+    plantName == "Black haw" ~ "Blackhaw",
+    plantName == "Blueberry vaccinium sp." ~ "Vaccinium",
+    plantName == "Box elder (acer negundo)" ~ "Acer negundo",
+    plantName == "Boxelder maple" ~ "Acer negundo",
+    plantName == "Burr oak" ~ "Bur oak",
+    plantName == "Bush honeysuckle (caprifoliaceae family)" ~ "Bush honeysuckle",
+    plantName == "Buttonwood-mangrove" ~ "Button mangrove",
+    plantName == "California-laurel" ~ "California laurel",
+    plantName == "Carolina cherry laurel" ~ "Carolina lauralcherry",
+    plantName == "Crab apple sp" ~ "Malus",
+    plantName == "Crepe myrtle" ~ "Crapemyrtle",
+    plantName == "Eastern sweet shrub" ~ "Eastern sweetshrub",
+    plantName == "Fermont cottonwood" ~ "Fremont's cottonwood",
+    plantName == "Fraser magnolia" ~ "Fraser's magnolia",
+    plantName == "Japanese spindle tree" ~ "Japanese spindletree",
+    plantName == "Japanese cherry" ~ "Japanese flowering cherry",
+    plantName == "Lombardy poplar" ~ "Lombardy's poplar",
+    plantName == "Maple leaf viburnum" ~ "Mapleleaf viburnum",
+    plantName == "Mountain or fraser magnolia" ~ "Fraser's magnolia",
+    plantName == "Nothoscordum gracile (onionweed)" ~ "Nothoscordum gracile",
+    plantName == "Honey suckle" ~ "Honeysuckle",
+    plantName == "Hop hornbeam" ~ "Hophornbeam",
+    plantName == "Hop-hornbeam" ~ "Hophornbeam",
+    plantName == "Hydrangeas" ~ "Hydrangea",
+    plantName == "Olive quihoui??" ~ "NA",
+    plantName == "Pear tree" ~ "Pear",
+    plantName == "Red bay" ~ "Redbay",
+    plantName == "Prunus spp.(apricot)" ~ "Prunus",
+    plantName == "Prunus spp.(cherry)" ~ "Prunus",
+    plantName == "Prunus spp.(plum)" ~ "Prunus",
+    plantName == "Prunus serulata (japanese cherry blossom)" ~ "Prunus serultaa",
+    plantName == "Quihoui???" ~ "NA",
+    plantName == "Red osier dogwood" ~ "Redosier dogwood",
+    plantName == "Red-osier dogwood" ~ "Redosier dogwood",
+    plantName == "Red osier dogwood (cornus sericea)" ~ "Redosier dogwood",
+    plantName == "Sweet gum" ~ "Sweetgum",
+    plantName == "Simplocos tinctoria" ~ "Symplocos tinctoria",
+    plantName == "Symac?" ~ "NA",
+    plantName == "Symplocos tinctotria" ~ "Symplocos tinctoria",
+    plantName == "Trembling aspen" ~ "Quaking aspen",
+    plantName == "Vaccinum corymbosum" ~ "Vaccinium corymbosum",
+    plantName == "Virburnum" ~ "Viburnum",
+    plantName == "Wild cherry" ~ "Prunus",
+    plantName == "Winged eonymous" ~ "Winged Euonymus",
+    plantName %in% c("", "8", "9", "N/A", "Unknown") ~ "NA",
+    TRUE ~ plantName),
+    cleanedPlantName = str_replace(cleanedPlantName, " spp.$", ""),
+    cleanedPlantName = str_replace(cleanedPlantName, " spp$", ""),
+    cleanedPlantName = str_replace(cleanedPlantName, " sp$", ""),
+    cleanedPlantName = str_replace(cleanedPlantName, " sp.$", ""),
+    cleanedPlantName = str_replace(cleanedPlantName, " species$", "")#,
+    #cleanedPlantName = str_replace(cleanedPlantName, "?", "")
+  )
+###Original Code###
+sites = read.csv(list.files()[grep('Site.csv', list.files())], header = TRUE, stringsAsFactors = FALSE)
+plants = read.csv(list.files()[grep('Plant.csv', list.files())], header = TRUE, stringsAsFactors = FALSE)
+
+coniferList = unique(plants[, c('Species', 'IsConifer')])
+
 plantspp = data.frame(plantName = unique(plants$Species)) %>%
   mutate(plantName = as.character(plantName),
          cleanedPlantName = case_when(
@@ -15,62 +87,43 @@ plantspp = data.frame(plantName = unique(plants$Species)) %>%
            plantName == "Arrowwood" ~ "Viburnum",
            plantName == "Arrowwood viburnum" ~ "Viburnum",
            plantName == "Big-leaf dogwood" ~ "Cornus macrophylla",
-           plantName == "Black haw" ~ "Blackhaw",
            plantName == "Blueberry vaccinium sp." ~ "Vaccinium",
            plantName == "Box elder (acer negundo)" ~ "Acer negundo",
            plantName == "Boxelder maple" ~ "Acer negundo",
            plantName == "Burr oak" ~ "Bur oak",
            plantName == "Bush honeysuckle (caprifoliaceae family)" ~ "Bush honeysuckle",
-           plantName == "Buttonwood-mangrove" ~ "Button mangrove",
            plantName == "California-laurel" ~ "California laurel",
-           plantName == "Carolina cherry laurel" ~ "Carolina lauralcherry",
            plantName == "Crab apple sp" ~ "Malus",
            plantName == "Crepe myrtle" ~ "Crapemyrtle",
            plantName == "Eastern sweet shrub" ~ "Eastern sweetshrub",
-           plantName == "Fermont cottonwood" ~ "Fremont's cottonwood",
            plantName == "Fraser magnolia" ~ "Fraser's magnolia",
-           plantName == "Japanese spindle tree" ~ "Japanese spindletree",
-           plantName == "Japanese cherry" ~ "Japanese flowering cherry",
-           plantName == "Lombardy poplar" ~ "Lombardy's poplar",
-           plantName == "Maple leaf viburnum" ~ "Mapleleaf viburnum",
            plantName == "Mountain or fraser magnolia" ~ "Fraser's magnolia",
-           plantName == "Nothoscordum gracile (onionweed)" ~ "Nothoscordum gracile",
            plantName == "Honey suckle" ~ "Honeysuckle",
            plantName == "Hop hornbeam" ~ "Hophornbeam",
            plantName == "Hop-hornbeam" ~ "Hophornbeam",
            plantName == "Hydrangeas" ~ "Hydrangea",
-           plantName == "Olive quihoui??" ~ "NA",
            plantName == "Pear tree" ~ "Pear",
            plantName == "Red bay" ~ "Redbay",
-           plantName == "Prunus spp.(apricot)" ~ "Prunus",
-           plantName == "Prunus spp.(cherry)" ~ "Prunus",
-           plantName == "Prunus spp.(plum)" ~ "Prunus",
-           plantName == "Prunus serulata (japanese cherry blossom)" ~ "Prunus serultaa",
-           plantName == "Quihoui???" ~ "NA",
            plantName == "Red osier dogwood" ~ "Redosier dogwood",
            plantName == "Red-osier dogwood" ~ "Redosier dogwood",
            plantName == "Red osier dogwood (cornus sericea)" ~ "Redosier dogwood",
            plantName == "Sweet gum" ~ "Sweetgum",
            plantName == "Simplocos tinctoria" ~ "Symplocos tinctoria",
-           plantName == "Symac?" ~ "NA",
            plantName == "Symplocos tinctotria" ~ "Symplocos tinctoria",
            plantName == "Trembling aspen" ~ "Quaking aspen",
            plantName == "Vaccinum corymbosum" ~ "Vaccinium corymbosum",
            plantName == "Virburnum" ~ "Viburnum",
            plantName == "Wild cherry" ~ "Prunus",
-           plantName == "Winged eonymous" ~ "Winged Euonymus",
            plantName %in% c("", "8", "9", "N/A", "Unknown") ~ "NA",
            TRUE ~ plantName),
          cleanedPlantName = str_replace(cleanedPlantName, " spp.$", ""),
          cleanedPlantName = str_replace(cleanedPlantName, " spp$", ""),
          cleanedPlantName = str_replace(cleanedPlantName, " sp$", ""),
          cleanedPlantName = str_replace(cleanedPlantName, " sp.$", ""),
-         cleanedPlantName = str_replace(cleanedPlantName, " species$", "")#,
-         #cleanedPlantName = str_replace(cleanedPlantName, "?", "")
-         ) 
-
-
-
+         cleanedPlantName = str_replace(cleanedPlantName, " species$", ""),
+         cleanedPlantName = str_replace(cleanedPlantName, "\\?", "")
+  ) %>%
+  left_join(coniferList, by = c('plantName' = 'Species'))
 # Finding matching taxonomic entity in ITIS
 plantList = data.frame(cleanedName = unique(plantspp$cleanedPlantName[plantspp$cleanedPlantName != "NA"]), 
                        sciName = NA, 
@@ -99,6 +152,8 @@ for (i in 1:nrow(plantList)) {
 
 #Finding new species by comparing the cleanedNames in the plantListcomparison.csv and cleanedPlantList.csv
 combined = plantListcomparison %>% filter(!cleanedName %in% cleanedPlantList$cleanedName)
+
+cleancombined = cleanednewSpeciesList %>% filter(!cleanedPlantName %in% cleanedPlantList$cleanedName)
 
 #Trying to find the conifers in the newSpeciesList
 coniferList = unique(newSpeciesList[, c('cleanedName', 'isConifer')])
