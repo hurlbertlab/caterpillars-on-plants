fcef683c913886a7a5f3e95b0221c745fa1fa711
Allen Hurlbert <hurlbert@bio.unc.edu>
2022-03-11 09:48:04 -0500

    summarizing plant traits

--- TRY/practice.R
+++ TRY/practice.R
@@ -2,27 +2,37 @@
 
 library(readxl)
 library(dplyr)
+library(tidyr)
 
 # Read in TRY trait files
-try = read_excel("TRY/TRY_flaggeddataset.xls")
+try1 = read.csv('TRY/part1-fromTRYfromplantspp.csv')
+try2 = read.csv('TRY/part2-fromTRYfromplantspp.csv')
 
-leaflength = try %>%
-  filter(TraitName == "Leaf length") %>%
-  #  mutate(lengthCM = case_when(
-  #    OrigUnitStr == "cm" ~ OrigValueStr,
-  #    OrigUnitStr == "mm" ~ OrigValueStr/10,
-  #    OrigUnitStr == "m" ~ OrigValueStr*100
-  #  )) %>%
-  mutate(lengthCM = as.numeric(OrigValueStr)) %>%
-  group_by(AccSpeciesName) %>%
-  summarize(maxLength = max(lengthCM, na.rm = T),
-            meanLength = mean(lengthCM, na.rm = T),
-            minLength = min(lengthCM, na.rm = T))
-
-# Example incorporating multiple trait names
-filter(TraitName %in% c("Leaf length", "Leaf length excluding petiole (leaf lamina length)"))
+try = rbind(try1, try2)
 
 
+traitsummary = try %>%
+  filter(TraitName != "") %>%
+  mutate(NewTraitName = case_when(
+    TraitName %in% c("Leaf length", "Leaf length excluding petiole (leaf lamina length)") ~ "LeafLength",
+    TraitName %in% c("Leaf area (in case of compound leaves undefined if leaf or leaflet, undefined if petiole is in- or excluded)", 
+                     "Leaf area (in case of compound leaves: leaf, petiole excluded)", 
+                     "Leaf area (in case of compound leaves: leaf, undefined if petiole in- or excluded)", 
+                     "Leaf area (in case of compound leaves: leaflet, petiole excluded)", 
+                     "Leaf area (in case of compound leaves: leaflet, undefined if petiole is in- or excluded)") ~ "LeafArea"),
+    StdValue = as.numeric(StdValue)) %>%
+  group_by(AccSpeciesName, NewTraitName) %>%
+  summarize(max = max(StdValue, na.rm = T),
+            mean = mean(StdValue, na.rm = T),
+            min = min(StdValue, na.rm =T)) 
+  
+lengths = filter(traitsummary, NewTraitName == "LeafLength")
+areas = filter(traitsummary, NewTraitName == "LeafArea")
+
+traits = left_join(lengths, areas, by = 'AccSpeciesName') %>%
+  rename(maxLeafLength = max.x,
+         meanLeafLength = mean.x, ...) %>%
+  select(AccSpeciesName, maxLeafLength, ...)
 
 ----------------------------------------------------------------------------------
   
@@ -76,16 +86,15 @@ library(readxl)
 library(dplyr)
 
 alltraits_summaries_part1 = part1_fromTRYfromplantspp %>%
-  filter(TraitName %in% c("Leaf area (in case of compound leaves undefined if leaf or leaflet, undefined if petiole is in- or excluded)")) %>%
-  mutate(lengthCM = case_when(
-    # when the original value (which is in the units in OrigUnitStr) is in mm2 then replace it with the orginal value divided by 100 = cm2
-    UnitName == "mm2" ~ StdValue/100,
-  )) %>%
-  mutate(lengthCM = as.numeric(StdValue)) %>%
+  filter(TraitName %in% c("Leaf area (in case of compound leaves undefined if leaf or leaflet, undefined if petiole is in- or excluded)", 
+                          "Leaf area (in case of compound leaves: leaf, petiole excluded)",
+                          "Leaf area (in case of compound leaves: leaf, undefined if petiole in- or excluded)",
+                          "Leaf area (in case of compound leaves: leaflet, petiole excluded)",
+                          "Leaf area (in case of compound leaves: leaflet, undefined if petiole is in- or excluded)")) %>%
   group_by(AccSpeciesName) %>%
-  summarize(maxLength = max(lengthCM, na.rm = T),
-            meanLength = mean(lengthCM, na.rm = T),
-            minLength = min(lengthCM, na.rm = T))
+  summarize(maxLeafArea = max(StdValue, na.rm = T),
+            meanLeafArea = mean(StdValue, na.rm = T),
+            minLeafArea = min(StdValue, na.rm = T))
 
 # Example incorporating multiple trait names
 filter(TraitName %in% c("Leaf length", "Leaf length excluding petiole"))
