11db6383a98d6e00d24b866f71cf8f0ee9005265
colleenw17 <cw17@live.unc.edu>
2022-04-12 11:01:35 -0400

    adding to the r script and joining to tallamay

--- mean_density_by_plantspecies.r
+++ mean_density_by_plantspecies.r
@@ -1,6 +1,19 @@
+library(tidyverse)
+library(lubridate)
+library(data.table)
+library(gsheet)
+library(maps)
+library(sp)
+library(maptools)
 
+#Added bc "mutate_cond" is not a built-in function
+mutate_cond <- function(.data, condition,...,envir=parent.frame()){
+  condition <- eval(substitute(condition),.data,envir)
+  .data[condition,] <- .data[condition,] %>% mutate(...)
+  .data
+}
 
-meanDensityByPlant = function(surveyData, # merged dataframe of Survey and arthropodSighting tables for a single site
+meanDensityBySpecies = function(surveyData, # merged dataframe of Survey and arthropodSighting tables for a single site
                              ordersToInclude = 'All',       # or 'caterpillar'
                              
                              minLength = 0,         # minimum arthropod size to include 
@@ -21,26 +34,39 @@ meanDensityByPlant = function(surveyData, # merged dataframe of Survey and arthr
     filter(julianday >= jdRange[1], julianday <= jdRange[2]) %>%
     mutate(julianweek = 7*floor(julianday/7) + 4)
   
-  effortByPlantSpecies = firstFilter %>%
-    group_by(PlantSpecies) %>%  ## group_by(PlantSpecies)  or group_by(Species)
+  effortBySpecies = firstFilter %>%
+    group_by(Species) %>%  ## group_by(PlantSpecies)  or group_by(Species)
     summarize(nSurveys = n_distinct(ID))
   
   arthCount = firstFilter %>%
     filter(Length >= minLength, 
            Group %in% ordersToInclude) %>%
     mutate(Quantity2 = ifelse(Quantity > outlierCount, 1, Quantity)) %>% #outlier counts replaced with 1
-    group_by(PlantSpecies) %>%
+    group_by(Species) %>%
     summarize(totalCount = sum(Quantity2, na.rm = TRUE),
               numSurveysGTzero = length(unique(ID[Quantity > 0])),
               totalBiomass = sum(Biomass_mg, na.rm = TRUE)) %>% 
-    right_join(effortByPlantSpecies, by = 'PlantSpecies') %>%
+    right_join(effortBySpecies, by = 'Species') %>%
     #next line replaces 3 fields with 0 if the totalCount is NA
     mutate_cond(is.na(totalCount), totalCount = 0, numSurveysGTzero = 0, totalBiomass = 0) %>%
     mutate(meanDensity = totalCount/nSurveys,
            fracSurveys = 100*numSurveysGTzero/nSurveys,
            meanBiomass = totalBiomass/nSurveys) %>%
-    arrange(PlantSpecies) %>%
+  x  arrange(Species) %>%
     data.frame()
   
   return(arthCount)
 }
+
+#Joining so "Species" becomes associated with a sciName
+caterpillar_unclean = read.csv('Tasks/caterpillar_plantanalysis.csv')
+plants_clean = read.csv('Tasks/plantList_rerun.csv')
+cleaned <- left_join(caterpillar_unclean, plants_clean, by= 'Species')
+
+#Created a column with just "Genus" in order to add to "tallamy_shrop...csv"
+Genus <- word(cleaned$sciName, 1)
+cleanedish <- mutate(cleaned, Genus=Genus)
+
+tallamy = read.csv('data/tallamy_shropshire_2009_plant_genera.csv')
+clean_and_tallamy <- left_join(tallamy, cleanedish, by = 'Genus')
+clean_and_tallamy <- select(clean_and_tallamy, -"X.1", -"X.2", -"X.x")
