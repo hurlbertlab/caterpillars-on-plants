5bc4ae05dd3dfeaa4031967b46a3ccb84f3e5b6a
colleenw17 <cw17@live.unc.edu>
2022-10-26 23:33:49 -0400

    two versions of the same script were created, looked at both to see which was most recent, deleted the least recent copy, and moved the most recent one into the folder

--- PlantsToIdentify/IDforPlantsThatAreNotIdentified.R
+++ PlantsToIdentify/IDforPlantsThatAreNotIdentified.R
@@ -19,6 +19,8 @@ surveys = read.csv(paste(github_raw, filter(data_links, grepl("Survey.csv", file
 
 plants = read.csv(paste(github_raw, filter(data_links, grepl("Plant.csv", file_name))$file_name, sep = ''), header = TRUE, stringsAsFactors = FALSE)
 
+ArthropodSighting = read.csv(paste(github_raw, filter(data_links, grepl("ArthropodSighting.csv", file_name))$file_name, sep = ''), header = TRUE, stringsAsFactors = FALSE)
+
 # Filtering plants to where Species is N/A and obtaining that ID
 unidentifiedBranches <- filter(plants, plants$Species == "N/A") %>%
                         select(ID, Species)
@@ -26,21 +28,54 @@ unidentifiedBranches <- filter(plants, plants$Species == "N/A") %>%
 # Filtering surveys to find where PlantSpecies has a name entered by a user
 # and comparing to where Species has not been identified
 userIdentifiedBranches <- surveys %>%
-  filter(PlantSpecies != "N/A", 
-         PlantSpecies != "", 
-         PlantSpecies != "Dvt", 
-         PlantSpecies != "Dvz", 
-         PlantSpecies != "Dvt", 
-         PlantSpecies != "N/a", 
-         PlantSpecies != "Tree", 
-         PlantSpecies != "Unknown", 
-         PlantSpecies != "Unknown, will take picture") %>%
+  filter(!PlantSpecies %in% c("N/A","","Hello","Dvt","Dvz","Dvt","N/a","Tree","Unknown","Unknown, will take picture")) %>%
   select(UserFKOfObserver, PlantSpecies, PlantFK) %>%
   left_join(unidentifiedBranches, by = c('PlantFK' = 'ID')) %>%
   filter(Species == "N/A") %>%
-  # Giving a confidence rating (1 is the least confident, 3 is the most) the most agreed upon name given by users
+  group_by(PlantFK) %>%
+  summarize(PlantSpecies = paste(PlantSpecies, collapse = ", ")) %>%
+  # Giving a confidence rating for the most agreed upon name given by users 
+  # 1 is the least confident meaning disagreement, 2 means only one name ever entered,
+  # 3 is the most confident with all entries agreeing multiple times)
   mutate(InferredName = NA, 
          ConfidenceInterval = NA, 
          Notes = NA)
-  # In Excel, fill in the inferred name if there's agreement and confidence rating
+
+write.csv(userIdentifiedBranches, "PlantsToIdentify/userIdentifiedBranches.csv", row.names = F)
+#### where did UserofFKofObserver go? if it's added it creates multiple rows with the same UserID
+
+# In Excel, fill in the inferred name if there's agreement and confidence rating
+# Obtain the branches with photos as they have an iNat ID and try to ID the plant from the photo
+ratedUserIdentifiedBranches <- read.csv(file = 'PlantsToIdentify/userIdentifiedBranches.csv')
+  #to join Name and Region from sites.R, have to find a column in plants or surveys, etc. to do so (find common column)
+
+branchesWithPhotos <- surveys %>%
+  filter(PlantFK %in% ratedUserIdentifiedBranches$PlantFK,
+         ObservationMethod == "Visual") %>% 
+  left_join(ArthropodSighting, by = c('ID' = 'SurveyFK')) %>%
+  left_join(plants, by = c('PlantFK' = 'ID')) %>%
+  left_join(sites, by = c('SiteFK' = 'ID')) %>%
+  filter(PhotoURL != "") %>% 
+  select(Name, Region, PlantFK, PlantSpecies, Notes.x, PhotoURL)
+
+write.csv(branchesWithPhotos, "PlantsToIdentify/branchesWithPhotos.csv", row.names = F)
+#added in Excel, InferredName and myNotes column to try and use the iNaturalist photos to determine plant species
+
+#think through logic of all the things we want to look at
+#don't need two sets of code just the one with stuff you think would be useful
+allbranchesWithPhotos <- surveys %>%
+  filter(ObservationMethod == "Visual") %>% 
+  left_join(ArthropodSighting, by = c('ID' = 'SurveyFK')) %>%
+  left_join(plants, by = c('PlantFK' = 'ID')) %>%
+  filter(Species == "N/A") %>%
+  left_join(sites, by = c('SiteFK' = 'ID')) %>%
+  filter(PhotoURL != "") %>% 
+  select(Name, Region, PlantFK, PlantSpecies, Notes.x, PhotoURL)
+
+
+
+# In Excel, look at the PhotoURL and paste it after https://caterpillarscount.unc.edu/images/arthropods/
+# Try to ID plants based off of photos and add to InferredName in ratedUserIdentifiedBranches.csv
+
+IDBranchesWithPhotos <- read.csv(file = 'PlantsToIdentify/brancheWithPhotos.csv', row.names = F)
 
